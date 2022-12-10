5ea9c573a12e98bb0a566d3a31190e11682c7270
Praveen <praveenk9417@gmail.com>
2022-12-10 17:04:03 +0530

    Submit encapsulation Assignment

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
-  
+
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+        obj.setName("praveen");
+        System.out.println(obj.getName());
+    }
 }
+//'name' has private access in 'com.driver.RWOnly':7
+//Cannot resolve symbol 'name':8
